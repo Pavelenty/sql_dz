fab33384bfa790b895b104e4be4dba0768d0178e
PavelPRO <PavelPRO2003.07@gmail.com>
2022-03-30 00:57:13 +0300

    I started creating a search

--- main.py
+++ main.py
@@ -6,17 +6,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    columns_bd = ['show_id', 'title', 'country', 'release_year', 'listed_in', 'description']
+    films_bd = query_bd(columns_bd, 'release_year >= 2020', 100)
+    """
+    Дальнейшую часть кода я хотел сделать функцией
+    но в голову не пришла годная идея как это хорошо сделать, 
+    поэтому оставил так
+    """
+    films_dict = {}
+    for row in films_bd:
+        films_dict[row[0]] = {
+            'title': row[1],
+            'country': row[2],
+            'release': row[3],
+            'genre': row[4],
+            'description': row[5]
+        }
+    return render_template('index.html', search=films_dict)
 
 @app.route('/Advanced_Search', methods=["GET", "POST"])
 def Advanced_Search():
-    search_get = request.form.get('test')
-    print(search_get)
+    columns_bd = ['title', 'country']
+    filter = []
+    post_list = ['name_film', 'country_film']
+    for post_req in post_list:
+        if request.form.get(post_req) != '':
+            if post_req == 'name_film':
+                filter.append(f'title LIKE "{request.form.get(post_req)}%"')
+            if post_req == 'country':
+                filter.append(f'country == "{request.form.get(post_req)}"')
+    print(filter)
+    films_search = query_bd(columns_bd, filter, 100)
+    for row in films_search:
+        print(row)
     return render_template('Advanced_Search.html')
 
 
-@app.route('/DZ')
+@app.route('/DZ', methods=["GET", "POST"])
 def DZ():
     return render_template('DZ.html')
 
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
 
-def query_bd(name_columns, filters, name_bd = 'netflix'):
+def query_bd(name_columns, filters, limit=0, name_bd = 'netflix'):
     if isinstance(name_columns, list):
         name_columns_convert = ", ".join(name_columns)
     else:
@@ -11,10 +11,16 @@ def query_bd(name_columns, filters, name_bd = 'netflix'):
     else:
         filters_convert = filters
 
+    if limit == 0:
+        q_limit = ''
+    else:
+        q_limit = f'LIMIT {limit}'
+
     query = f"""
                 SELECT {name_columns_convert}
                 FROM {name_bd}
                 WHERE {filters_convert}
+                {q_limit};
             """
 
     with sql.connect("data/netflix.db") as connect:
