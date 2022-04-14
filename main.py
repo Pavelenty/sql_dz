f37c43dd26ad64fc68509e38bb66a24572af5bfd
PavelPRO <PavelPRO2003.07@gmail.com>
2022-04-14 10:59:58 +0300

    DZ

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import sqlite3 as sql
-from utils import query_bd
+from utils import query_bd, filtered_list, search_genre
 
 app = Flask(__name__)
 
@@ -29,7 +29,8 @@ def Advanced_Search():
     columns_bd = ['show_id', 'title', 'country', 'listed_in', 'rating', 'release_year', 'description']
     filter = []
     post_list = ['name_film', 'country_film', 'genre', 'rating', 'release_year_one', 'release_year_two']
-    genre_list = ['Любой', 'TV Thrillers', 'TV Action & Adventure', 'Action & Adventure', 'Faith & Spirituality', 'Spanish-Language TV Shows', 'Science & Nature TV', 'International TV Shows', 'TV Dramas', 'Reality TV', "Kids' TV", 'Crime TV Shows', 'Movies', 'British TV Shows', 'TV Sci-Fi & Fantasy', 'Documentaries', 'Docuseries', 'LGBTQ Movies', 'Stand-Up Comedy & Talk Shows', 'Korean TV Shows', 'Romantic TV Shows', 'Anime Series', 'Anime Features', 'Sci-Fi & Fantasy', 'Stand-Up Comedy', 'Horror Movies', 'Thrillers', 'Dramas', 'TV Shows', 'Classic & Cult TV', 'Teen TV Shows', 'Children & Family Movies', 'TV Horror', 'Sports Movies', 'International Movies', 'TV Mysteries', 'TV Comedies', 'Comedies', 'Romantic Movies', 'Independent Movies', 'Music & Musicals']
+    genre_list = filtered_list('listed_in')
+    genre_list = ['Любой'] + genre_list
     for post_req in post_list:
         if request.form.get(post_req) != '' and request.form.get(post_req) != 'Любой':
             if post_req == 'name_film':
@@ -37,7 +38,7 @@ def Advanced_Search():
             if post_req == 'country':
                 filter.append(f'country == "{request.form.get(post_req)}"')
             if post_req == 'genre':
-                filter.append(f'listed_in == "{request.form.get(post_req)}"')
+                filter.append(f'listed_in LIKE "{request.form.get(post_req)}%"')
             if post_req == 'rating':
                 filter.append(f'rating == "{request.form.get(post_req)}"')
             if post_req == 'release_year_one':
@@ -59,10 +60,55 @@ def Advanced_Search():
     return render_template('Advanced_Search.html', genre_list=genre_list, search=films_dict)
 
 
-@app.route('/DZ', methods=["GET", "POST"])
-def DZ():
-    return render_template('DZ.html')
+@app.route('/movie/<title>', methods=["GET", "POST"])
+@app.route('/movie', methods=["GET", "POST"])
+def DZ(title='d'):
+    name = request.form.get('name_film')
+    columns_bd = ['show_id', 'title', 'country', 'release_year', 'listed_in', 'description']
+    films_bd = query_bd(columns_bd, f'title LIKE "{title}%"', 100)
+    films_dict = {}
+    for row in films_bd:
+        films_dict[row[0]] = {
+            'title': row[1],
+            'country': row[2],
+            'release': row[3],
+            'genre': row[4],
+            'description': row[5]
+        }
+    return render_template('DZ.html', search=films_dict, name=name)
 
+
+@app.route('/rating/<rat>', methods=["GET", "POST"])
+def rating(rat):
+    rating_list = []
+    if rat == 'children':
+        rating_list = ['G']
+    if rat == 'family':
+        rating_list = ['G', 'PG', 'PG-13']
+    if rat == 'adult':
+        rating_list = ['R', 'NC-17']
+    filter = []
+    for rating in rating_list:
+        filter.append(f'rating == "{rating}"')
+    columns_bd = ['show_id', 'title', 'country', 'release_year', 'listed_in', 'description']
+    films_bd = query_bd(columns_bd, filter, 100, 'OR')
+
+    films_dict = {}
+    for row in films_bd:
+        films_dict[row[0]] = {
+            'title': row[1],
+            'country': row[2],
+            'release': row[3],
+            'genre': row[4],
+            'description': row[5]
+        }
+    return render_template('index.html', search=films_dict)
+
+
+@app.route('/genre/<genre>')
+def genre_search(genre):
+    desired_element = search_genre(genre)
+    return render_template('genre.html', list=desired_element)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import sqlite3 as sql
 
-def query_bd(name_columns, filters, limit=0, name_bd = 'netflix'):
+def query_bd(name_columns, filters, limit=0, operation='AND', name_bd = 'netflix'):
     if isinstance(name_columns, list):
         name_columns_convert = ", ".join(name_columns)
     else:
         name_columns_convert = name_columns
 
     if isinstance(filters, list):
-        filters_convert = " AND ".join(filters)
+        filters_convert = f" {operation} ".join(filters)
     else:
         filters_convert = filters
 
@@ -26,3 +26,26 @@ def query_bd(name_columns, filters, limit=0, name_bd = 'netflix'):
     with sql.connect("data/netflix.db") as connect:
         cursor = connect.cursor()
     return cursor.execute(query)
+
+def filtered_list(column):
+    genre_list = []
+    bd_column = query_bd(column, f'{column} <> ""')
+    for el in bd_column:
+        el_list = el[0].split(',')
+        for el_el in el_list:
+            el_el = el_el.replace(' ', '')
+            genre_list.append(el_el)
+    genre_list = set(genre_list)
+    return list(genre_list)
+
+
+def search_genre(genre):
+    list_search_genre = []
+    bd_column = query_bd('title, description', f'listed_in LIKE "{genre}%"', 10)
+    for el in bd_column:
+        list_search_genre.append({
+            "title": el[0],
+            "description": el[1]
+        })
+    return list_search_genre
+
